ef4b2496caa6c095a6dc04802e5e8fe26403d7ff
Nathaniel <pastika@cern.ch>
2015-12-10 18:09:46 -0600

    Strip trailing white spaces

--- Tools/derivedTupleVariables.h
+++ Tools/derivedTupleVariables.h
@@ -83,7 +83,7 @@ namespace plotterFunctions
                         //set to not overflow histograms
                         if(mu1pt >= 2000.0) mu1pt = 1999.9;
                         if(mu2pt >= 2000.0) mu2pt = 1999.9;
-                        
+
                         //Get mu efficiencies
                         double muEff1 = 0.0, muEff2 = 0.0;
 
@@ -92,8 +92,8 @@ namespace plotterFunctions
                             //Fit to reco eff (eff = p0 + p1*pt + p2*pt^2
                             //PHYS14
                             //const double fitStart = 200.0; // extended to 1400 GeV
-                            //const double p0 =     0.955847; // +/- 0.461944    
-                            //const double p1 = -2.24431e-05; // +/- 0.00128305  
+                            //const double p0 =     0.955847; // +/- 0.461944
+                            //const double p1 = -2.24431e-05; // +/- 0.00128305
                             //const double p2 = -5.68907e-08; // +/- 7.85913e-07
                             //Sprint15
                             //const double fitStart = 200.0; // extended to 1400 GeV
@@ -101,9 +101,9 @@ namespace plotterFunctions
                             //const double p1 = -7.81897e-06; // +/- 4.16487e-03
                             //const double p2 = -1.22092e-08; // +/- 2.18556e-06
                             const double fitStart = 200.0;  // extended to 2000 GeV
-                            const double p0 =     0.979238; //   +/-   1.17313     
-                            const double p1 = -6.47338e-06; //   +/-   0.00342715  
-                            const double p2 = -1.16258e-08; //   +/-   1.87837e-06 
+                            const double p0 =     0.979238; //   +/-   1.17313
+                            const double p1 = -6.47338e-06; //   +/-   0.00342715
+                            const double p2 = -1.16258e-08; //   +/-   1.87837e-06
 
                             double muRecoEff = 0.0;
 
@@ -127,8 +127,8 @@ namespace plotterFunctions
                             muEff2 = muRecoEff * muEffIso->GetBinContent(isoPtBin, isoActBin);
                         }
 
-                        
-                        if((mu1pt > 20 && muEff1 < 1.0e-5) || (mu2pt > 20 && muEff2 < 1.0e-5)) 
+
+                        if((mu1pt > 20 && muEff1 < 1.0e-5) || (mu2pt > 20 && muEff2 < 1.0e-5))
                         {
                             std::cout << "SMALL muEff!!! muEff1: " << muEff1 << "\tmuEff2: " << muEff2 << "\t" << mu1pt << "\t" << mu2pt << std::endl;
                             zEff = 1.0e-10;
@@ -166,34 +166,34 @@ namespace plotterFunctions
                         {
                             //Fit to iso eff (eff = p0 + p1*pt + p2*pt^2
                             const double fitStart = 200.0;  // extended to 2000 GeV
-                            const double p0 =     0.989411; //   +/-   1.18467     
-                            const double p1 =  3.66321e-06; //   +/-   0.00346729  
-                            const double p2 = -5.68292e-09; //   +/-   1.90334e-06 
+                            const double p0 =     0.989411; //   +/-   1.18467
+                            const double p1 =  3.66321e-06; //   +/-   0.00346729
+                            const double p2 = -5.68292e-09; //   +/-   1.90334e-06
 
                             double elecIsoEff = 0.0;
 
                             //int isoPtBin = elecEffIso->GetXaxis()->FindBin(elec1pt);
                             //if(elec1pt > fitStart) elecIsoEff = p0 + p1*elec1pt + p2*elec1pt*elec1pt;
                             //else                   elecIsoEff = elecEffIso->GetBinContent(isoPtBin);
-                        
+
                             int recoPtBin = elecEffReco->GetXaxis()->FindBin(elec1pt);
                             if(recoPtBin >= elecEffReco->GetNbinsX()) recoPtBin = elecEffReco->GetNbinsX();
-                        
+
                             int recoActBin = elecEffReco->GetYaxis()->FindBin(elec1Act);
                             if(recoActBin >= elecEffReco->GetNbinsY()) recoActBin = elecEffReco->GetNbinsY();
-                        
+
                             elecIsoEff = elecEffIso->GetBinContent(recoPtBin, recoActBin);
                             elecEff1 = elecIsoEff * elecEffReco->GetBinContent(recoPtBin, recoActBin);
                             //std::cout << elec1pt << "\t" << recoPtBin << "\t" <<  elec1Act << "\t" << recoActBin << "\t" << elecEff1 << "\t" << elecIsoEff << "\t" << elecEffReco->GetBinContent(recoPtBin, recoActBin) << std::endl;
-                        
+
                             elecIsoEff = 0.0;
                             //isoPtBin = elecEffIso->GetXaxis()->FindBin(elec2pt);
                             //if(elec2pt > fitStart) elecIsoEff = p0 + p1*elec2pt + p2*elec2pt*elec2pt;
                             //else                   elecIsoEff = elecEffIso->GetBinContent(isoPtBin);
-                        
+
                             recoPtBin = elecEffReco->GetXaxis()->FindBin(elec2pt);
                             if(recoPtBin >= elecEffReco->GetNbinsX()) recoPtBin = elecEffReco->GetNbinsX();
-                        
+
                             recoActBin = elecEffReco->GetYaxis()->FindBin(elec2Act);
                             if(recoActBin >= elecEffReco->GetNbinsY()) recoActBin = elecEffReco->GetNbinsY();
 
@@ -202,8 +202,8 @@ namespace plotterFunctions
                             //std::cout << elec2pt << "\t" << recoPtBin << "\t" << elec2Act << "\t" << recoActBin << "\t" << elecEff2 << "\t" << elecIsoEff << "\t" << elecEffReco->GetBinContent(recoPtBin, recoActBin) << std::endl;
                         }
 
-                        
-                        if((elec1pt > 33 && elec2pt) && (elecEff1 < 1.0e-5 || elecEff2 < 1.0e-5)) 
+
+                        if((elec1pt > 33 && elec2pt) && (elecEff1 < 1.0e-5 || elecEff2 < 1.0e-5))
                         {
                             std::cout << "SMALL elecEff!!! elecEff1: " << elecEff1 << "\telecEff2: " << elecEff2 << "\t" << elec1pt << "\t" << elec2pt << std::endl;
                             zEffElec = 1.0e-10;
@@ -229,12 +229,12 @@ namespace plotterFunctions
 
                 if(jet.Pt() > MHT_jetPtMin) MHT += jet;
             }
-        
+
             double mudR = 99.9, genMudR = 99.9, genMudPhi = 99.9, genMudEta = 99.9;
 
             if(cutMuVec.size() >= 2) mudR    = ROOT::Math::VectorUtil::DeltaR(cutMuVec[0], cutMuVec[1]);
             if(genMu.size() > 2) std::cout << "MORE THAN 2 GEN MUONS: " << genMu.size() << std::endl;
-            if(genMu.size() >= 2)    
+            if(genMu.size() >= 2)
             {
                 genMudR = ROOT::Math::VectorUtil::DeltaR(*(genMu[0]), *(genMu[1]));
                 genMudPhi = ROOT::Math::VectorUtil::DeltaPhi(*(genMu[0]), *(genMu[1]));
@@ -251,24 +251,24 @@ namespace plotterFunctions
             double acc_p0 = -2.64921e-01;
             double acc_p1 = -4.65305e-03;
             double acc_p2 =  9.50493e-01;
-        
-            if(hZAcc && hZAccLepPt) 
+
+            if(hZAcc && hZAccLepPt)
             {
                 //Eta portion of acceptance
                 if(bestRecoZPt < 100) zAcc = hZAcc->GetBinContent(hZAcc->GetXaxis()->FindBin(bestRecoZPt));
                 else                  zAcc = acc_p2 - exp(acc_p0 + acc_p1 * bestRecoZPt);
 
-                //pt portion of acceptance 
+                //pt portion of acceptance
                 zAcc *= hZAccLepPt->GetBinContent(hZAccLepPt->GetXaxis()->FindBin(bestRecoZPt));
             }
 
-            if(passMuZinvSel && zAcc < 0.05) 
+            if(passMuZinvSel && zAcc < 0.05)
             {
                 std::cout << "WARNING: Muon Z acceptance < 0.05, forcing weight to zero! Acc_Z: " << zAcc << std::endl;
                 zAcc = 1.0e101;
             }
 
-            if(passMuZinvSel && zEff < 0.04) 
+            if(passMuZinvSel && zEff < 0.04)
             {
                 std::cout << "WARNING: Muon Z efficiency < 0.05, forcing weight to zero! Eff_Z: " << zEff << "\tZ(Pt): " << bestRecoZPt <<  std::endl;
                 zEff = 1.0e101;
@@ -283,24 +283,24 @@ namespace plotterFunctions
             double elecAcc_p0 = -2.50679e-01;
             double elecAcc_p1 = -5.34976e-03;
             double elecAcc_p2 = 9.33562e-01;
-        
+
             if(hZAccElec && hZAccLepPtElec)
             {
                 //Eta portion of the acceptance
                 if(bestRecoZPt < 100) zAccElec = hZAccElec->GetBinContent(hZAccElec->GetXaxis()->FindBin(bestRecoZPt));
                 else                  zAccElec = elecAcc_p2 - exp(elecAcc_p0 + elecAcc_p1 * bestRecoZPt);
 
-                //Pt portion of the acceptance 
+                //Pt portion of the acceptance
                 zAccElec = hZAccLepPtElec->GetBinContent(hZAccLepPtElec->GetXaxis()->FindBin(bestRecoZPt));
             }
 
-            if(passElecZinvSel && zAccElec < 0.05) 
+            if(passElecZinvSel && zAccElec < 0.05)
             {
                 std::cout << "WARNING: Elec Z acceptance < 0.05, forcing weight to zero! Acc_Z: " << zAccElec << std::endl;
                 zAccElec = 1.0e101;
             }
 
-            if(passElecZinvSel && zEffElec < 0.04) 
+            if(passElecZinvSel && zEffElec < 0.04)
             {
                 std::cout << "WARNING: Elec Z efficiency < 0.05, forcing weight to zero! Eff_Z: " << zEffElec << "\tZ(Pt): " << bestRecoZPt <<  std::endl;
                 zEffElec = 1.0e101;
@@ -497,7 +497,7 @@ namespace plotterFunctions
             const std::vector<int>& W_emuVec                = tr.getVec<int>("W_emuVec");
             const std::vector<double>& muonsCharge          = tr.getVec<double>("muonsCharge");
             const std::vector<TLorentzVector>& jetsLVec     = tr.getVec<TLorentzVector>("jetsLVec");
-            const std::vector<double>& recoJetschargedEmEnergyFraction     = tr.getVec<double>("recoJetschargedEmEnergyFraction"); 
+            const std::vector<double>& recoJetschargedEmEnergyFraction     = tr.getVec<double>("recoJetschargedEmEnergyFraction");
             const std::vector<double>& recoJetschargedHadronEnergyFraction = tr.getVec<double>("recoJetschargedHadronEnergyFraction");
             const std::vector<int> & muonsFlagIDVec = tr.getVec<int>("muonsFlagMedium");
             const std::vector<int>&  elesFlagIDVec  = tr.getVec<int>("elesFlagVeto");
@@ -529,7 +529,7 @@ namespace plotterFunctions
             {
                 //std::cout << "void muInfo(NTupleReader& tr): Caught exception, variable \"" << e << "\" not found" << std::endl;
             }
-            
+
             std::vector<const TLorentzVector*>* genMatchIsoElecInAcc = new std::vector<const TLorentzVector*>();
             std::vector<const TLorentzVector*>* genMatchElecInAcc = new std::vector<const TLorentzVector*>();
             std::vector<double>* genMatchElecInAccRes = new std::vector<double>();
@@ -561,18 +561,18 @@ namespace plotterFunctions
 
             std::vector<TLorentzVector> cutMuVecRecoOnly;
 
-            //muon selections 
+            //muon selections
             int sumMuCharge = 0;
             int nTriggerMuons = 0;
             for(int i = 0; i < muonsLVec.size(); ++i)
             {
-                if(AnaFunctions::passMuon( muonsLVec[i], 0.0, 0.0, muonsFlagIDVec[i], AnaConsts::muonsMiniIsoArr)) // emulates muons with pt but no iso requirements.  
+                if(AnaFunctions::passMuon( muonsLVec[i], 0.0, 0.0, muonsFlagIDVec[i], AnaConsts::muonsMiniIsoArr)) // emulates muons with pt but no iso requirements.
                 {
                     cutMuVecRecoOnly.push_back(muonsLVec[i]);
                 }
                 if(AnaFunctions::passMuon( muonsLVec[i], muonsMiniIso[i], 0.0, muonsFlagIDVec[i], AnaConsts::muonsMiniIsoArr))
                 {
-                    if(AnaFunctions::passMuon( muonsLVec[i], muonsRelIso[i], 0.0, muonsFlagIDVec[i], AnaConsts::muonsMiniIsoArr)) 
+                    if(AnaFunctions::passMuon( muonsLVec[i], muonsRelIso[i], 0.0, muonsFlagIDVec[i], AnaConsts::muonsMiniIsoArr))
                     {
                         if(nTriggerMuons == 0 && muonsLVec[i].Pt() > 17)  nTriggerMuons++;
                         else if(muonsLVec[i].Pt() > 8)  nTriggerMuons++;
@@ -589,7 +589,7 @@ namespace plotterFunctions
             int sumElecCharge = 0;
             for(int i = 0; i < elesLVec.size(); ++i)
             {
-                if(AnaFunctions::passElectron(elesLVec[i], 0.0, -1, elesisEB[i], elesFlagIDVec[i], AnaConsts::elesMiniIsoArr)) // emulates muons with pt but no iso requirements.  
+                if(AnaFunctions::passElectron(elesLVec[i], 0.0, -1, elesisEB[i], elesFlagIDVec[i], AnaConsts::elesMiniIsoArr)) // emulates muons with pt but no iso requirements.
                 {
                     cutElecVecRecoOnly.push_back(elesLVec[i]);
                 }
@@ -602,10 +602,10 @@ namespace plotterFunctions
                     if(elesCharge[i] > 0) sumElecCharge++;
                     else                  sumElecCharge--;
                 }
-            }        
+            }
 
 
-            //mu45 non-iso trigger emulation 
+            //mu45 non-iso trigger emulation
             const double effsnom2012ABC[] = {0.928,0.8302,0.8018};
             const double upedge2012ABC[] = { 0.9, 1.2, 2.1};
             bool muTrigMu45 = false;
@@ -624,14 +624,14 @@ namespace plotterFunctions
                     if(muTrigMu45) break;
                 }
             }
-        
+
             double genHt = 0.0;
 
             const double   minMuPt = 20.0,   highMuPt = 45.0;
             const double minElecPt = 33.0, highElecPt = 33.0;
             double nuPt1 = -999.9, nuPt2 = -999.9;
 
-            //Gen info parsing 
+            //Gen info parsing
             if(&genDecayPdgIdVec != nullptr && &genDecayLVec != nullptr)
             {
                 for(int i = 0; i < genDecayPdgIdVec.size() && i < genDecayLVec.size(); ++i)
@@ -645,8 +645,8 @@ namespace plotterFunctions
                 for(int index = 0; index < W_emuVec.size(); ++index)
                 {
                     int i = W_emuVec[index];
-                
-                    //muon efficiency and acceptance 
+
+                    //muon efficiency and acceptance
                     if(abs(genDecayPdgIdVec[i]) == 13)
                     {
                         genMu->push_back(&genDecayLVec[i]);
@@ -672,7 +672,7 @@ namespace plotterFunctions
                                 genMatchMuInAccAct->push_back(genMuAct->back());
                                 genMatchMuInAccRes->push_back((genDecayLVec[i].Pt() - matchPt)/genDecayLVec[i].Pt());
                             }
-                    
+
                             dRMin = 999.9;
                             for(int j = 0; j < cutMuVec->size(); ++j)
                             {
@@ -716,7 +716,7 @@ namespace plotterFunctions
                                 genMatchElecInAccAct->push_back(genElecAct->back());
                                 genMatchElecInAccRes->push_back((genDecayLVec[i].Pt() - matchPt)/genDecayLVec[i].Pt());
                             }
-                    
+
                             dRMin = 999.9;
                             for(int j = 0; j < cutElecVec->size(); ++j)
                             {
@@ -769,7 +769,7 @@ namespace plotterFunctions
             }
 
             bool passDiMuTrig  = nTriggerMuons >= 2;
-        
+
             const double zMassMin = 81.0;
             const double zMass    = 91.0;
             const double zMassMax = 101.0;
@@ -833,7 +833,7 @@ namespace plotterFunctions
 
             TLorentzVector bestRecoZ = (fabs(bestRecoElecZ.M() - zMass) > fabs(bestRecoMuZ.M() - zMass))?(bestRecoMuZ):(bestRecoElecZ);
             if(fabs(bestRecoZ.M() - zMass) > fabs(bestRecoElMuZ.M() - zMass)) bestRecoZ = bestRecoElMuZ;
-        
+
             metZ.SetPtEtaPhiM(bestRecoZ.Pt(), 0.0, bestRecoZ.Phi(), 0.0);
             TLorentzVector cleanMet = metV + metZ;
 
@@ -902,7 +902,7 @@ namespace plotterFunctions
             tr.registerDerivedVar("ngenMatchMuInAcc", static_cast<double>(genMatchMuInAcc->size()));
             tr.registerDerivedVec("genMatchMuInAccAct", genMatchMuInAccAct);
             tr.registerDerivedVec("genMatchIsoMuInAccAct", genMatchIsoMuInAccAct);
-        
+
             tr.registerDerivedVec("genElec", genElec);
             tr.registerDerivedVar("ngenElec", static_cast<double>(genElec->size()));
             tr.registerDerivedVec("genElecInAcc", genElecInAcc);
@@ -922,7 +922,7 @@ namespace plotterFunctions
             tr.registerDerivedVar("pdgIdZDec", pdgIdZDec);
             tr.registerDerivedVar("passDiMuIsoTrig", passDiMuTrig);
             tr.registerDerivedVar("passSingleMu45", muTrigMu45);
-        
+
             tr.registerDerivedVar("passDiMuSel", passDiMuSel);
             tr.registerDerivedVar("passDiElecSel", passDiElecSel);
 	    tr.registerDerivedVar("passElMuSel", passElMuSel);
@@ -1062,9 +1062,9 @@ namespace plotterFunctions
             std::vector<double> * nb0BinsNW = new std::vector<double>();
 
             //weights based on total N(b) yields vs. N(b) = 0 control region
-            //These weights are derived from the rato of events in the N(t) = 1, 2, 3 bins after all baseline cuts except b tag between the 
+            //These weights are derived from the rato of events in the N(t) = 1, 2, 3 bins after all baseline cuts except b tag between the
             //N(b) = 0 control region and each N(b) signal region using Z->nunu MC.  They account for both the combinatoric reweighting factor
-            //as well as the different event yields between the control region and each signal region.  
+            //as well as the different event yields between the control region and each signal region.
             const double wnb01 = 3.820752e-02;//3.478840e-02;//6.26687e-2;
             const double wnb02 = 2.946461e-03;//2.586369e-03;//5.78052e-3;
             const double wnb03 = 1.474770e-04;//1.640077e-04;//7.08235e-4;
@@ -1073,7 +1073,7 @@ namespace plotterFunctions
             const double w1b = wnb01 * weight1fakeb;
             const double w2b = wnb02 * weight2fakeb;
             const double w3b = wnb03 * weight3fakeb;
-	
+
             if(cntCSVS == 0)
             {
                 //nb0Bins->push_back(std::make_pair(find_Binning_Index(0, nTopCandSortedCnt, MT2, cleanMet), 1.0));
@@ -1110,7 +1110,7 @@ namespace plotterFunctions
 	int indexElecTrigger;
 
         void triggerInfo(NTupleReader& tr)
-        { 
+        {
             const std::vector<std::string>& triggerNames = tr.getVec<std::string>("TriggerNames");
             const std::vector<int>& passTrigger          = tr.getVec<int>("PassTrigger");
 
@@ -1147,7 +1147,7 @@ namespace plotterFunctions
 	    {
 		std::cout << "Could not find trigger in the list of trigger names" << std::endl;
 	    }
-	    
+
 	    tr.registerDerivedVar("passMuTrigger",passMuTrigger);
 	    tr.registerDerivedVar("passElecTrigger",passElecTrigger);
         }
@@ -1240,7 +1240,7 @@ namespace plotterFunctions
     //
     //    //if(met > 1000) std::cout << "run: " << run << "\tevent: " << event << "\tmet: " << met << "\tmetphi: " << metphi << "\tnMuons_CUT: " << nMuons_CUT << "\t nElectrons_CUT: " << nElectrons_CUT << "\tcntNJetsPt30: " << cntNJetsPt30 << "\tcntNJetsPt30Eta24: " << cntNJetsPt30Eta24 << "\tcntNJetsPt50Eta24: " << cntNJetsPt50Eta24 << "\tmht: " << mht << "\tmhtphi: " << mhtphi << "\tht: " << ht << std::endl;
     //}
-    
+
 }
 
 #endif
