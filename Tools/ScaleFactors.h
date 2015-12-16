9bd2abd7df62a80443f1d34017a801efa954db64
Nadja <nadja.strobbe@cern.ch>
2015-12-15 18:39:46 -0600

    Implement the normalization scale factor in small class

---
+++ Tools/ScaleFactors.h
@@ -0,0 +1,17 @@
+#ifndef SCALEFACTORS_H
+#define SCALEFACTORS_H
+
+class ScaleFactors
+{
+public:
+    static double sf_norm0b() 
+    {
+	return 0.729;
+    }
+    static double sfunc_norm0b() 
+    {
+	return 0.082;
+    }
+};
+
+#endif
