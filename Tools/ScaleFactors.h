3a6a2b7ea24ce34f9a3d794e3b524f7e68e9c53b
Nathaniel <pastika@cern.ch>
2016-07-24 12:33:40 -0500

    Update weights to 12 ifb

--- Tools/ScaleFactors.h
+++ Tools/ScaleFactors.h
@@ -6,11 +6,11 @@ class ScaleFactors
 public:
     static double sf_norm0b()
     {
-	return 0.777;
+	return 0.783;
     }
     static double sfunc_norm0b()
     {
-	return 0.084;
+	return 0.065;
     }
 };
 
