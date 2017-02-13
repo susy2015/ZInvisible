3f6afc5e1d5b425bf5c520d4834591f9e556f968
Nadja <nadja.strobbe@cern.ch>
2017-02-13 15:15:23 -0600

    update ttbar and rnorm scale factor computation

--- Tools/makeReweightingHisto.py
+++ Tools/makeReweightingHisto.py
@@ -135,7 +135,7 @@ def njetWeights(filename):
     fout.cd()
 
     # How to rebin
-    bins = [0,1,2,3,4,5,6,7,8,20]
+    bins = [0,1,2,3,4,5,6,7,20]
     bins_TT = [0,1,2,3,4,5,6,20]
 
     # Run over the relevant histograms
@@ -208,7 +208,7 @@ def njetWeights(filename):
 
         # apply weights to ttbar
         #h2s[1] = reweight(h2s[1], SFs["TT_elmuZinv"])#"TT_%s"%(cut.replace("mu","elmu"))])#weight applied to Njet for the Rnorm or DY correction
-        h2s[1] = h2s[1].Scale( SFs["TT_%s"%(cut.replace("mu","elmu"))].GetBinContent(1) )
+        h2s[1].Scale( SFs["TT_%s"%(cut.replace("mu","elmu"))].GetBinContent(1) )
 
         # subtract relevant histograms from data
         data_subtracted = subtract(h1, h2s[1:])
@@ -331,29 +331,29 @@ def normWeightwithReweight(f, SFs):
         print h1_g1b.Integral()
         h2s_g1b = [f.Get(sel) for sel in hnames2_DY_g1b]
 
-        h1test = f.Get(hname1%{"cut":cut, "selection":selection2})
-        print h1test.Integral()
+        #h1test = f.Get(hname1%{"cut":cut, "selection":selection2})
+        #print h1test.Integral()
 
         # apply weights to DY
         h2s_0b[0]  = reweight(h2s_0b[0],  SFs["DY_muZinv_0b"])
         h2s_g1b[0] = reweight(h2s_g1b[0], SFs["DY_muZinv_g1b"])
 
         # apply weights to ttbar
-        h2s_0b[1]=reweight(h2s_0b[1], SFs["TT_elmuZinv_g1b"])#.Scale(0.92)
-        h2s_g1b[1]=reweight(h2s_g1b[1], SFs["TT_elmuZinv_g1b"])#.Scale(0.92)
-        print "why",h2s_0b[0]
+        h2s_0b[1].Scale(SFs["TT_elmuZinv_0b"].GetBinContent(1))#.Scale(0.92)
+        h2s_g1b[1].Scale(SFs["TT_elmuZinv_g1b"].GetBinContent(1))#.Scale(0.92)
+
         # Combine histograms
-        h1 = h1_0b.Clone()
-        h1.Add(h1_g1b)
+        h1 = h1_g1b.Clone()
+        h1.Add(h1_0b)
         h2s = []
         for i in xrange(len(h2s_0b)):
-            h2s.append(h2s_0b[i].Clone())
-            h2s[i].Add(h2s_g1b[i])
+            h2s.append(h2s_g1b[i].Clone())
+            h2s[i].Add(h2s_0b[i])
 
         # subtract relevant histograms from data
         data_subtracted = subtract(h1, h2s[1:])
         #print data_subtracted.Integral()
-        newname = "DataMC_nb_%s_%s"%(cut,selection2)
+        newname = "DataMC_nj_%s_%s"%(cut,selection2)
         newh = makeRatio(data_subtracted, [h2s[0]], newname=newname, bins=[0,20])
         #newh = makeRatio(h1, h2s, newname=newname)
 
