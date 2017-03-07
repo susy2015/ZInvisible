49a735a5d66590f94044d6fa0446472095ad303a
Scarlet Norberg <scarletnorberg2014@gmail.com>
2017-03-07 17:52:10 -0600

    checking why things are notworking

--- Tools/ScaleFactors.h
+++ Tools/ScaleFactors.h
@@ -6,11 +6,11 @@ class ScaleFactors
 public:
     static double sf_norm0b()
     {
-	return 0.991;
+	return 1.034;
     }
     static double sfunc_norm0b()
     {
-	return 0.068;
+	return 0.084;
     }
 };
 
--- Tools/ScaleFactorsttBar.h
+++ Tools/ScaleFactorsttBar.h
@@ -6,7 +6,7 @@ class ScaleFactorsttBar
 public:
     static double sf_norm0b()
     {
-        return 1.065;
+        return 0.950;
     }
     static double sfunc_norm0b()
     {
