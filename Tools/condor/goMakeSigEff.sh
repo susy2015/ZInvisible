b4e7a689ce9cb352b352cc2fd9608935f20bad16
Nathaniel <pastika@cern.ch>
2017-05-05 00:09:40 -0500

    Added goMakeSigEff.sh

---
+++ Tools/condor/goMakeSigEff.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+export PATH=${PATH}:/cvmfs/cms.cern.ch/common
+export CMS_PATH=/cvmfs/cms.cern.ch
+
+#get the release setup and in place
+tar -xzf $2.tar.gz
+cd $2/
+mkdir -p src
+cd src
+scram b ProjectRename
+eval `scramv1 runtime -sh`
+
+#set up local code
+tar -xzf ${_CONDOR_SCRATCH_DIR}/gmse.tar.gz
+cd WORLDSWORSESOLUTIONTOAPROBLEM
+
+export LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:${PWD}
+
+echo "xrdcp root://cmseos.fnal.gov/$(echo $6 | sed 's|/eos/uscms||') ."
+xrdcp root://cmseos.fnal.gov/$(echo $6 | sed 's|/eos/uscms||') .
+
+ls -lhrt
+
+export LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:${_CONDOR_SCRATCH_DIR}
+
+./makeSignalHistograms --condor -D $1 -N $3 -M $4 
+
+ls -lhrt
+
+mv signalHists_* ${_CONDOR_SCRATCH_DIR}
+
+rm $(echo $6 | sed 's|.*/||')
+rm -r ${_CONDOR_SCRATCH_DIR}/$2
+
