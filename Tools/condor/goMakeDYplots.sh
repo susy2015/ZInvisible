8b7748b1532269e899113414e3be4f737ac8cb38
caleb-james-smith <caleb_smith2@baylor.edu>
2018-06-13 09:53:35 -0500

    add condor scripts

---
+++ Tools/condor/goMakeDYplots.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+export PATH=${PATH}:/cvmfs/cms.cern.ch/common
+export CMS_PATH=/cvmfs/cms.cern.ch
+
+#get the release setup and in place
+tar -xzf $2.tar.gz
+cd $2/
+mkdir -p src
+cd src
+scram b ProjectRename
+eval `scramv1 runtime -sh`
+
+#set up local code
+tar -xzf ${_CONDOR_SCRATCH_DIR}/gmp.tar.gz
+cd WORLDSWORSESOLUTIONTOAPROBLEM
+
+export LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:${PWD}
+
+echo "xrdcp root://cmseos.fnal.gov/$(echo $6 | sed 's|/eos/uscms||') ."
+xrdcp root://cmseos.fnal.gov/$(echo $6 | sed 's|/eos/uscms||') .
+
+ls -lhrt
+
+#./makeDataMCplots -st --condor -D $1 -N $3 -M $4 -L $5 -S SB_v1_2017
+./makeDYplots -st --condor -D $1 -N $3 -M $4 -L $5 -S SB_v1_2017 
+
+ls -lhrt
+
+mv histoutput_* ${_CONDOR_SCRATCH_DIR}
+mv minituple_histoutput_* ${_CONDOR_SCRATCH_DIR}
+
+rm $(echo $6 | sed 's|.*/||')
+rm -r ${_CONDOR_SCRATCH_DIR}/$2
