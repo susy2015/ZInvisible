1b5bb2a6938663b5b63feb26ff5f12a69e8e1724
caleb-james-smith <caleb_smith2@baylor.edu>
2018-08-02 15:16:58 -0500

    update Makefile with include directory; remove include directory from derivedTupleVariables.h

--- Tools/derivedTupleVariables.h
+++ Tools/derivedTupleVariables.h
@@ -1,19 +1,19 @@
 #ifndef DERIVEDTUPLEVARIABLES_H
 #define DERIVEDTUPLEVARIABLES_H
 
-#include "include/Ak8DrMatch.h"
-#include "include/Fakebtagvectors.h"
-#include "include/Gamma.h"
-#include "include/GenerateWeight.h"
-#include "include/GetSearchBin.h"
-#include "include/LepInfo.h"
-#include "include/MetSmear.h"
-#include "include/NJetAk8.h"
-#include "include/NJetWeight.h"
-#include "include/PrepareMiniTupleVars.h"
-#include "include/SystematicCalc.h"
-#include "include/SystematicPrep.h"
-#include "include/Taudiv.h"
-#include "include/TriggerInfo.h"
+#include "Ak8DrMatch.h"
+#include "Fakebtagvectors.h"
+#include "Gamma.h"
+#include "GenerateWeight.h"
+#include "GetSearchBin.h"
+#include "LepInfo.h"
+#include "MetSmear.h"
+#include "NJetAk8.h"
+#include "NJetWeight.h"
+#include "PrepareMiniTupleVars.h"
+#include "SystematicCalc.h"
+#include "SystematicPrep.h"
+#include "Taudiv.h"
+#include "TriggerInfo.h"
 
 #endif
