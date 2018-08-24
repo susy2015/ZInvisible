cb35291e59bafc4f1c361fc7a59241dcfaf77baf
caleb-james-smith <caleb_smith2@baylor.edu>
2018-08-23 19:10:57 -0500

    update process condor output script: better printing and check for root files in condor directory

--- Tools/condor/processResults.sh
+++ Tools/condor/processResults.sh
@@ -12,12 +12,16 @@
 # - make plots using summed result
 
 
+
 dataSet=$1
 
 if [ -z "$dataSet" ]; then
-    echo "ERROR: Please provide a data set for the directory name."
+    echo "- ERROR: Please provide a data set for the directory name."
+    echo "  Examples: DYJetsToLL, GJets, ZJetsToNuNu, etc."
     exit 1
 fi
+
+echo "- Running processResults.sh for the data set $dataSet"
 
 # ZInvisible directory
 zinvDir=$CMSSW_BASE/src/ZInvisible/Tools
@@ -39,20 +43,26 @@ resultFile="result.root"
 # go to condor directory
 cd $condorDir
 
+# check if there are any root files here
+if [[ ! $(ls *.root) ]]; then
+    echo "- ERROR: There are no root files in the directory $condorDir to process."
+    exit 1
+fi
+
 # check if data directory exists
 while [[ -d $dataDir ]]
 do
-    echo "Found directory $dataDir"
+    echo "- Found directory $dataDir"
     i=$[$i+1]
     dataDir="histos_"$1"_"$today"_"$i""
 done
 
 # create unique data directory
-echo "Creating directory $dataDir"
+echo "- Creating directory $dataDir"
 mkdir $dataDir
 
 # move root files to data directory
-echo "Moving root files to $dataDir"
+echo "- Moving root files to $dataDir"
 mv *.root $dataDir
 
 # add histograms
@@ -61,46 +71,47 @@ error=1
 # while there are errors, attempt hadd
 while [[ $error != 0 ]]
 do
+    echo "- Executing hadd to add histograms; see hadd.log for output"
     hadd $resultFile *.root &> hadd.log
     error=$?
     if [[ $error != 0 ]]; then
-        echo "ERROR: hadd command failed"
+        echo "  ERROR: hadd command failed"
         if [[ ! -d $brokenDir ]]; then
             mkdir $brokenDir
         fi
         substring="hadd exiting due to error in"
         message=$(grep "$substring" hadd.log)
         file=$(expr match "$message" "$substring \(.*\)")
-        echo "message: $message"
-        echo "file: $file"
+        echo "  message: $message"
+        echo "  file: $file"
         mv $file $brokenDir
         rm $resultFile
     else
-        echo "hadd command succeeded"
+        echo "  hadd command succeeded"
     fi
 done
 
-echo "Copying $resultFile to $zinvDir"
+echo "- Copying $resultFile to $zinvDir"
 if [[ -f $resultFile ]]; then
     cp $resultFile $zinvDir
 else
-    echo "ERROR: the file $resultFile does not exit"
+    echo "  ERROR: the file $resultFile does not exit"
     exit 1
 fi
 
-echo "Removing old plots"
+echo "- Removing old plots"
 cd $zinvDir
 rm plots/*
 
-echo "Compiling MakePlots"
+echo "- Compiling MakePlots"
 make -j8
 
-echo "Running MakePlots"
+echo "- Running MakePlots"
 ./makePlots -f -I $resultFile
 
 if [[ $? == 0 ]]; then
-    echo "MakePlots was successful"
+    echo "  MakePlots was successful"
 else
-    echo "ERROR: MakePlots failed"
+    echo "  ERROR: MakePlots failed"
 fi
 
