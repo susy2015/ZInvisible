5c5255e753b0abe08a27098a66875c3f377d5fe9
caleb-james-smith <caleb_smith2@baylor.edu>
2018-09-06 11:34:27 -0500

    Header file to define data type in one place

---
+++ Tools/include/TypeDefinitions.h
@@ -0,0 +1,9 @@
+// TypeDefinitions.h
+#ifndef TYPEDEFINITIONS_H
+#define TYPEDEFINITIONS_H
+
+// typedef and using both create type aliases
+// typedef has template limitations, but using does not
+using data_t = double;
+
+#endif
