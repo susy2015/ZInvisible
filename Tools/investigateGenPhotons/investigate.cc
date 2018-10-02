4a96d922517bd8757e9c953b56ac114e2aa1c5f1
caleb-james-smith <caleb_smith2@baylor.edu>
2018-10-02 15:46:39 -0500

    DrawOptions class

--- Tools/investigateGenPhotons/investigate.cc
+++ Tools/investigateGenPhotons/investigate.cc
@@ -13,14 +13,39 @@
 #include "TCanvas.h"
 #include "TTreeReader.h"
 
-void investigate(const char* inputFileName);
-void plot(TTree* tree, std::string plotName, const char * varexp, const char * selection);
+// simple class
+class DrawOptions
+{
+    public:
+        std::string plotName;
+        std::string varexp;
+        std::string selection;
+        int color;
+};
 
-void plot(TTree* tree, std::string plotName, const char * varexp, const char * selection)
+void plot(TTree* tree, DrawOptions p)
 {
     std::string plotDir = "plots/";
     TCanvas* c1 = new TCanvas("c","c", 600.0, 600.0);
-    tree->Draw(varexp, selection);
+    tree->Draw(p.varexp.c_str(), p.selection.c_str(), "E");
+    c1->SaveAs((plotDir + p.plotName + ".png").c_str());
+    c1->SaveAs((plotDir + p.plotName + ".pdf").c_str());
+    delete c1;
+}
+
+void multiplot(TTree* tree, std::vector<DrawOptions> vp, std::string plotName)
+{
+    // get htemp
+    TH1F *htemp = (TH1F*)gPad->GetPrimitive("htemp");
+    std::string plotDir = "plots/";
+    TCanvas* c1 = new TCanvas("c","c", 600.0, 600.0);
+    for (const auto & p : vp)
+    {
+        // draw
+        tree->GetXaxis()->SetRangeUser(-5.0, 5.0);
+        tree->SetLineColor(p.color);
+        tree->Draw(p.varexp.c_str(), p.selection.c_str(), "same E");
+    }
     c1->SaveAs((plotDir + plotName + ".png").c_str());
     c1->SaveAs((plotDir + plotName + ".pdf").c_str());
     delete c1;
@@ -29,7 +54,7 @@ void plot(TTree* tree, std::string plotName, const char * varexp, const char * s
 void investigate(const char* inputFileName)
 {
     // open that file please 
-    TFile* file = NULL;
+    TFile* file = nullptr;
     file = TFile::Open(inputFileName);
     if (!file)
     {
@@ -37,41 +62,58 @@ void investigate(const char* inputFileName)
         exit(1);
     }
     // get tree
-    const char* treeName = "Events";
-    TTree* tree = NULL;
-    tree = (TTree*)file->Get(treeName);
+    std::string treeName = "Events";
+    TTree* tree = nullptr;
+    tree = (TTree*)file->Get(treeName.c_str());
     if (!tree)
     {
-        printf("ERROR: Did not open tree %s\n", treeName);
+        printf("ERROR: Did not open tree %s\n", treeName.c_str());
         exit(1);
     }
     // make plots
+    std::vector<DrawOptions> plotOptions;
     std::string plotName = "";
-    const char * varexp = "";
-    const char * selection = "";
+    std::string varexp = "";
+    std::string selection = "";
     
-    plotName = "genStatus_pdgId=22_pt>10";
-    varexp = "recoGenParticles_prunedGenParticles__PAT.obj.status()";
-    selection = "recoGenParticles_prunedGenParticles__PAT.obj.pdgId() == 22 && recoGenParticles_prunedGenParticles__PAT.obj.pt() > 10.0";
-    plot(tree, plotName, varexp, selection);
+    // from Dr. Ken
+    // plotName = "genStatus_pdgId=22_pt>10";
+    // varexp = "recoGenParticles_prunedGenParticles__PAT.obj.status()";
+    // selection = "recoGenParticles_prunedGenParticles__PAT.obj.pdgId() == 22 && recoGenParticles_prunedGenParticles__PAT.obj.pt() > 10.0";
+    // DrawOptions p1 = {plotName, varexp, selection};
+    // plotOptions.push_back(p1);
     
     plotName = "genEta_eta<5";
     varexp = "recoGenParticles_prunedGenParticles__PAT.obj.eta()";
     selection = "abs(recoGenParticles_prunedGenParticles__PAT.obj.eta()) < 5.0";
-    plot(tree, plotName, varexp, selection);
+    DrawOptions p1 = {plotName, varexp, selection, kRed};
+    plotOptions.push_back(p1);
     
     plotName = "genEta_eta<5_pt>10";
     varexp = "recoGenParticles_prunedGenParticles__PAT.obj.eta()";
     selection = "abs(recoGenParticles_prunedGenParticles__PAT.obj.eta()) < 5.0 && recoGenParticles_prunedGenParticles__PAT.obj.pt() > 10.0";
-    plot(tree, plotName, varexp, selection);
+    DrawOptions p2 = {plotName, varexp, selection, kBlue};
+    plotOptions.push_back(p2);
     
     plotName = "genEta_eta<5_pt>10_pdgId=22";
     varexp = "recoGenParticles_prunedGenParticles__PAT.obj.eta()";
     selection = "abs(recoGenParticles_prunedGenParticles__PAT.obj.eta()) < 5.0 && recoGenParticles_prunedGenParticles__PAT.obj.pt() > 10.0 && recoGenParticles_prunedGenParticles__PAT.obj.pdgId() == 22";
-    plot(tree, plotName, varexp, selection);
+    DrawOptions p3 = {plotName, varexp, selection, kGreen-2};
+    plotOptions.push_back(p3);
     
+    // loop over plots
+    for (const auto& p : plotOptions)
+    {
+        plot(tree, p);
+    }
+    
+    // multiplot
+    multiplot(tree, plotOptions, "all_the_gen");
+
     // close that file please
     file->Close();
+    // delete file
+    delete file;
 }
 
 int main()
