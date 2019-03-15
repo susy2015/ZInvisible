2d3512eddfba44e78c64a0410702b163f6f7a853
caleb-james-smith <caleb_smith2@baylor.edu>
2019-03-15 16:47:20 -0500

    plot cutPhotonPt and metWithPhoton

--- Tools/calculateCorrectionFactors.py
+++ Tools/calculateCorrectionFactors.py
@@ -3,6 +3,7 @@
 import ROOT
 
 # file: condor/histos_PhotonAndMuonControlRegionSelection_2016_11_Mar_2019_2/result.root
+#
 # directory: cutPhotonPt
 # Low DM
 # KEY: TH1D    DataMC_Photon_LowDM_PhotonPt_2016cutPhotonPtcutPhotonPtDatadata
@@ -21,6 +22,25 @@ import ROOT
 # KEY: TH1D    DataMC_Photon_HighDM_PhotonPt_2016cutPhotonPtcutPhotonPttWstack
 # KEY: TH1D    DataMC_Photon_HighDM_PhotonPt_2016cutPhotonPtcutPhotonPtRarestack
 
+# directory: metWithPhoton
+# Low DM
+# KEY: TH1D  DataMC_Photon_LowDM_met_2016metWithPhotonmetWithPhotonDatadata
+# KEY: TH1D  DataMC_Photon_LowDM_met_2016metWithPhotonmetWithPhoton#gamma+jetsstack
+# KEY: TH1D  DataMC_Photon_LowDM_met_2016metWithPhotonmetWithPhotonQCDstack
+# KEY: TH1D  DataMC_Photon_LowDM_met_2016metWithPhotonmetWithPhotonW(l#nu)+jetsstack
+# KEY: TH1D  DataMC_Photon_LowDM_met_2016metWithPhotonmetWithPhotont#bar{t}stack
+# KEY: TH1D  DataMC_Photon_LowDM_met_2016metWithPhotonmetWithPhotontWstack
+# KEY: TH1D  DataMC_Photon_LowDM_met_2016metWithPhotonmetWithPhotonRarestack
+# High DM
+# KEY: TH1D  DataMC_Photon_HighDM_met_2016metWithPhotonmetWithPhotonDatadata
+# KEY: TH1D  DataMC_Photon_HighDM_met_2016metWithPhotonmetWithPhoton#gamma+jetsstack
+# KEY: TH1D  DataMC_Photon_HighDM_met_2016metWithPhotonmetWithPhotonQCDstack
+# KEY: TH1D  DataMC_Photon_HighDM_met_2016metWithPhotonmetWithPhotonW(l#nu)+jetsstack
+# KEY: TH1D  DataMC_Photon_HighDM_met_2016metWithPhotonmetWithPhotont#bar{t}stack
+# KEY: TH1D  DataMC_Photon_HighDM_met_2016metWithPhotonmetWithPhotontWstack
+# KEY: TH1D  DataMC_Photon_HighDM_met_2016metWithPhotonmetWithPhotonRarestack
+
+
 class Histogram:
 
     def __init__(self, directory, name):
@@ -29,6 +49,7 @@ class Histogram:
         self.histogram = 0
 
 def main():
+    plot_dir = "correctionFactors/"
     c = ROOT.TCanvas("c", "c", 800, 800)
     f_name = "condor/histos_PhotonAndMuonControlRegionSelection_2016_11_Mar_2019_2/result.root"
     f = ROOT.TFile(f_name)
@@ -37,83 +58,65 @@ def main():
         return
     # map simple keys to full histogram name
     h_map = {}
-    h_map["PhotonPt_Data_LowDM"]   = Histogram("cutPhotonPt", "DataMC_Photon_LowDM_PhotonPt_2016cutPhotonPtcutPhotonPtDatadata"          )
-    h_map["PhotonPt_GJets_LowDM"]  = Histogram("cutPhotonPt", "DataMC_Photon_LowDM_PhotonPt_2016cutPhotonPtcutPhotonPt#gamma+jetsstack"  )
-    h_map["PhotonPt_QCD_LowDM"]    = Histogram("cutPhotonPt", "DataMC_Photon_LowDM_PhotonPt_2016cutPhotonPtcutPhotonPtQCDstack"          )
-    h_map["PhotonPt_WJets_LowDM"]  = Histogram("cutPhotonPt", "DataMC_Photon_LowDM_PhotonPt_2016cutPhotonPtcutPhotonPtW(l#nu)+jetsstack" )
-    h_map["PhotonPt_TTbar_LowDM"]  = Histogram("cutPhotonPt", "DataMC_Photon_LowDM_PhotonPt_2016cutPhotonPtcutPhotonPtt#bar{t}stack"     )
-    h_map["PhotonPt_tW_LowDM"]     = Histogram("cutPhotonPt", "DataMC_Photon_LowDM_PhotonPt_2016cutPhotonPtcutPhotonPttWstack"           )
-    h_map["PhotonPt_Rare"]         = Histogram("cutPhotonPt", "DataMC_Photon_LowDM_PhotonPt_2016cutPhotonPtcutPhotonPtRarestack"         )
+    # --- cutPhotonPt --- #
+    h_map["cutPhotonPt_Data_LowDM"]   = Histogram("cutPhotonPt", "DataMC_Photon_LowDM_PhotonPt_2016cutPhotonPtcutPhotonPtDatadata"          )
+    h_map["cutPhotonPt_GJets_LowDM"]  = Histogram("cutPhotonPt", "DataMC_Photon_LowDM_PhotonPt_2016cutPhotonPtcutPhotonPt#gamma+jetsstack"  )
+    h_map["cutPhotonPt_QCD_LowDM"]    = Histogram("cutPhotonPt", "DataMC_Photon_LowDM_PhotonPt_2016cutPhotonPtcutPhotonPtQCDstack"          )
+    h_map["cutPhotonPt_WJets_LowDM"]  = Histogram("cutPhotonPt", "DataMC_Photon_LowDM_PhotonPt_2016cutPhotonPtcutPhotonPtW(l#nu)+jetsstack" )
+    h_map["cutPhotonPt_TTbar_LowDM"]  = Histogram("cutPhotonPt", "DataMC_Photon_LowDM_PhotonPt_2016cutPhotonPtcutPhotonPtt#bar{t}stack"     )
+    h_map["cutPhotonPt_tW_LowDM"]     = Histogram("cutPhotonPt", "DataMC_Photon_LowDM_PhotonPt_2016cutPhotonPtcutPhotonPttWstack"           )
+    h_map["cutPhotonPt_Rare_LowDM"]   = Histogram("cutPhotonPt", "DataMC_Photon_LowDM_PhotonPt_2016cutPhotonPtcutPhotonPtRarestack"         )
+    # --- metWithPhoton --- #
+    h_map["metWithPhoton_Data_LowDM"]   = Histogram("metWithPhoton", "DataMC_Photon_HighDM_met_2016metWithPhotonmetWithPhotonDatadata"          )
+    h_map["metWithPhoton_GJets_LowDM"]  = Histogram("metWithPhoton", "DataMC_Photon_HighDM_met_2016metWithPhotonmetWithPhoton#gamma+jetsstack"  )
+    h_map["metWithPhoton_QCD_LowDM"]    = Histogram("metWithPhoton", "DataMC_Photon_HighDM_met_2016metWithPhotonmetWithPhotonQCDstack"          )
+    h_map["metWithPhoton_WJets_LowDM"]  = Histogram("metWithPhoton", "DataMC_Photon_HighDM_met_2016metWithPhotonmetWithPhotonW(l#nu)+jetsstack" )
+    h_map["metWithPhoton_TTbar_LowDM"]  = Histogram("metWithPhoton", "DataMC_Photon_HighDM_met_2016metWithPhotonmetWithPhotont#bar{t}stack"     )
+    h_map["metWithPhoton_tW_LowDM"]     = Histogram("metWithPhoton", "DataMC_Photon_HighDM_met_2016metWithPhotonmetWithPhotontWstack"           )
+    h_map["metWithPhoton_Rare_LowDM"]   = Histogram("metWithPhoton", "DataMC_Photon_HighDM_met_2016metWithPhotonmetWithPhotonRarestack"         )
     for h_key in h_map:
         myHist = h_map[h_key]
         myHist.key = h_key
         myHist.histogram = f.Get(myHist.directory + "/" + myHist.name)
-        #myHist.histogram = ROOT.TH1F(f.Get(myHist.directory + "/" + myHist.name))
-        print "{0} histogram: type={1}, n_bins={2}".format(myHist.key, type(myHist.histogram), myHist.histogram.GetNbinsX())
+        #print "{0} histogram: type={1}, n_bins={2}".format(myHist.key, type(myHist.histogram), myHist.histogram.GetNbinsX())
         if not myHist.histogram:
             print "Unable to load the histogram for {0} named {1}".format(myHist.key, myHist.name)
-        #myHist.histogram.Draw()
-        #c.Update()
 
+    variables = ["cutPhotonPt", "metWithPhoton"]
+    for var in variables:
+        # Data
+        h_Data_LowDM = h_map[var + "_Data_LowDM"].histogram.Clone(var + "_Data_LowDM")
+        # MC
+        h_MC_LowDM   = h_map[var + "_GJets_LowDM"].histogram.Clone(var + "_GJets_LowDM")
+        h_MC_LowDM.Add(h_map[var + "_QCD_LowDM"].histogram)
+        h_MC_LowDM.Add(h_map[var + "_WJets_LowDM"].histogram)
+        h_MC_LowDM.Add(h_map[var + "_TTbar_LowDM"].histogram)
+        h_MC_LowDM.Add(h_map[var + "_tW_LowDM"].histogram)
+        h_MC_LowDM.Add(h_map[var + "_Rare_LowDM"].histogram)
+        # GJets
+        h_GJets_LowDM = h_map[var + "_GJets_LowDM"].histogram.Clone(var + "_GJets_LowDM")
+        # Background MC (without GJets)
+        h_Background_LowDM   = h_map[var + "_QCD_LowDM"].histogram.Clone(var + "_QCD_LowDM")
+        h_Background_LowDM.Add(h_map[var + "_WJets_LowDM"].histogram)
+        h_Background_LowDM.Add(h_map[var + "_TTbar_LowDM"].histogram)
+        h_Background_LowDM.Add(h_map[var + "_tW_LowDM"].histogram)
+        h_Background_LowDM.Add(h_map[var + "_Rare_LowDM"].histogram)
+        # Data minus background
+        h_DataMinusBackground_LowDM = h_map[var + "_Data_LowDM"].histogram.Clone(var + "_Data_LowDM")
+        h_DataMinusBackground_LowDM.Add(h_Background_LowDM, -1)
 
-
-    #h_map["PhotonPt_Data_LowDM"].histogram.Draw()
-
-    # Data
-    h_PhotonPt_Data_LowDM = h_map["PhotonPt_Data_LowDM"].histogram.Clone("PhotonPt_Data_LowDM")
-    # MC
-    h_PhotonPt_MC_LowDM   = h_map["PhotonPt_GJets_LowDM"].histogram.Clone("PhotonPt_GJets_LowDM")
-    h_PhotonPt_MC_LowDM.Add(h_map["PhotonPt_QCD_LowDM"].histogram)
-    h_PhotonPt_MC_LowDM.Add(h_map["PhotonPt_WJets_LowDM"].histogram)
-    h_PhotonPt_MC_LowDM.Add(h_map["PhotonPt_TTbar_LowDM"].histogram)
-    h_PhotonPt_MC_LowDM.Add(h_map["PhotonPt_tW_LowDM"].histogram)
-    h_PhotonPt_MC_LowDM.Add(h_map["PhotonPt_Rare"].histogram)
-    # GJets
-    h_PhotonPt_GJets_LowDM = h_map["PhotonPt_GJets_LowDM"].histogram.Clone("PhotonPt_GJets_LowDM")
-    # Background MC (without GJets)
-    h_PhotonPt_Background_LowDM   = h_map["PhotonPt_QCD_LowDM"].histogram.Clone("PhotonPt_QCD_LowDM")
-    h_PhotonPt_Background_LowDM.Add(h_map["PhotonPt_WJets_LowDM"].histogram)
-    h_PhotonPt_Background_LowDM.Add(h_map["PhotonPt_TTbar_LowDM"].histogram)
-    h_PhotonPt_Background_LowDM.Add(h_map["PhotonPt_tW_LowDM"].histogram)
-    h_PhotonPt_Background_LowDM.Add(h_map["PhotonPt_Rare"].histogram)
-    # Data minus background
-    h_PhotonPt_DataMinusBackground_LowDM = h_map["PhotonPt_Data_LowDM"].histogram.Clone("PhotonPt_Data_LowDM")
-    h_PhotonPt_DataMinusBackground_LowDM.Add(h_PhotonPt_Background_LowDM, -1)
- 
-
-
-    if not h_PhotonPt_Data_LowDM:
-        print "ERROR: h_PhotonPt_Data_LowDM not created"
-    if not h_PhotonPt_MC_LowDM:
-        print "ERROR: h_PhotonPt_MC_LowDM not created"
-
-    print "h_PhotonPt_Data_LowDM histogram: type={0}, n_bins={1}".format(type(h_PhotonPt_Data_LowDM), h_PhotonPt_Data_LowDM.GetNbinsX())
-    print "h_PhotonPt_MC_LowDM histogram: type={0}, n_bins={1}".format(type(h_PhotonPt_MC_LowDM), h_PhotonPt_MC_LowDM.GetNbinsX())
-    
-    h_PhotonPt_MC_LowDM.Draw()
-    h_PhotonPt_Data_LowDM.Draw("same")
-    c.Update()
-    c.SaveAs("PhotonPt_Data_MC.pdf")
-    
-    h_PhotonPt_GJets_LowDM.Draw()
-    h_PhotonPt_DataMinusBackground_LowDM.Draw("same")
-    c.Update()
-    c.SaveAs("PhotonPt_DataMinusBackground_GJets.pdf")
-
-def test():
-    myC = ROOT.TCanvas("myC", "myC", 800, 800)
-    myC.cd()
-    f_name = "condor/histos_PhotonAndMuonControlRegionSelection_2016_11_Mar_2019_2/result.root"
-    f = ROOT.TFile(f_name)
-    histogram = f.Get("cutPhotonPt" + "/" + "DataMC_Photon_LowDM_PhotonPt_2016cutPhotonPtcutPhotonPtDatadata")
-    print histogram.GetNbinsX()
-    histogram.Draw()
-    myC.Update()
-    #myC.SaveAs("myC.pdf")
+        h_MC_LowDM.Draw()
+        h_Data_LowDM.Draw("same")
+        c.Update()
+        c.SaveAs(plot_dir + var + "_Data_MC.pdf")
+        
+        h_GJets_LowDM.Draw()
+        h_DataMinusBackground_LowDM.Draw("same")
+        c.Update()
+        c.SaveAs(plot_dir + var + "_DataMinusBackground_GJets.pdf")
 
 if __name__ == "__main__":
     main()
-    #test()
 
 
 
