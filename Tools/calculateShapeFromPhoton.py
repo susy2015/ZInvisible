27205763da25479a4b2630ac78338c66de46448f
caleb-james-smith <caleb_smith2@baylor.edu>
2019-05-24 15:15:56 -0500

    introduce validation bins to shape factor

--- Tools/calculateShapeFromPhoton.py
+++ Tools/calculateShapeFromPhoton.py
@@ -64,6 +64,7 @@ class Shape:
     def getShape(self, file_name, era): 
         # currently the histograms are named by year (2018) and not era (2018_AB)
         # we should probalby change the histograms to use era (2018_AB)
+        draw_option = "hist error"
         year = era[0:4]
         eraTag = "_" + era
         # check that the file exists
@@ -82,6 +83,9 @@ class Shape:
         legend_x2 = 0.9 
         legend_y1 = 0.7 
         legend_y2 = 0.9 
+
+        xbin_300 = np.array([0.0, 250.0, 300.0, 1000.0])
+        xbin_400 = np.array([0.0, 250.0, 400.0, 1000.0])
         
         # setup histogram map
         self.setupHistoMap(year)
@@ -97,7 +101,7 @@ class Shape:
             h_TTbar = f.Get(self.variable + "/" + self.histos[year][region]["TTbar"])
             h_tW    = f.Get(self.variable + "/" + self.histos[year][region]["tW"])
             h_Rare  = f.Get(self.variable + "/" + self.histos[year][region]["Rare"])
-            
+             
             # add MC
             h_MC = h_GJets.Clone("h_MC") 
             h_MC.Add(h_QCD)
@@ -117,79 +121,101 @@ class Shape:
             h_MC_normalized = h_MC.Clone("h_MC_normalized")
             h_MC_normalized.Scale(ratio)
             
-            # ratios
-            h_ratio = h_Data.Clone("h_ratio")
-            h_ratio.Divide(h_MC)
-            h_ratio_normalized = h_Data.Clone("h_ratio_normalized")
-            h_ratio_normalized.Divide(h_MC_normalized)
+            # rebin
+            h_Data.Rebin(2)
+            h_MC.Rebin(2)
+            h_MC_normalized.Rebin(2)
+            h_Data_300 = h_Data.Rebin(3, "h_Data_300", xbin_300)
+            h_Data_400 = h_Data.Rebin(3, "h_Data_400", xbin_400)
+            h_MC_300 = h_MC.Rebin(3, "h_MC_300", xbin_300)
+            h_MC_400 = h_MC.Rebin(3, "h_MC_400", xbin_400)
+            h_MC_normalized_300 = h_MC_normalized.Rebin(3, "h_MC_normalized_300", xbin_300)
+            h_MC_normalized_400 = h_MC_normalized.Rebin(3, "h_MC_normalized_400", xbin_400)
+            h_map = {
+                        "standard" : {"data":h_Data,     "mc":h_MC,     "mc_norm":h_MC_normalized},
+                        "300"      : {"data":h_Data_300, "mc":h_MC_300, "mc_norm":h_MC_normalized_300},
+                        "400"      : {"data":h_Data_400, "mc":h_MC_400, "mc_norm":h_MC_normalized_400}
+            }
+            
+            for key in h_map:
+                keyTag = "_" + key
+                h_Data          = h_map[key]["data"]
+                h_MC            = h_map[key]["mc"]
+                h_MC_normalized = h_map[key]["mc_norm"]
+            
+                # ratios
+                h_ratio = h_Data.Clone("h_ratio")
+                h_ratio.Divide(h_MC)
+                h_ratio_normalized = h_Data.Clone("h_ratio_normalized")
+                h_ratio_normalized.Divide(h_MC_normalized)
         
-            # setup histograms
-            #setupHist(self, hist, title, x_title, y_title, color, y_min, y_max)
-            self.setupHist(h_Data,              self.variable + "_" + region + eraTag, self.label_met, "Events", self.color_red,   10.0 ** -1, 10.0 ** 6)
-            self.setupHist(h_MC,                self.variable + "_" + region + eraTag, self.label_met, "Events", self.color_blue,  10.0 ** -1, 10.0 ** 6)
-            self.setupHist(h_MC_normalized,     self.variable + "_" + region + eraTag, self.label_met, "Events", self.color_blue,  10.0 ** -1, 10.0 ** 6)
-            self.setupHist(h_ratio,             self.variable + "_" + region + eraTag, self.label_met, "Data/MC", self.color_black, 0.0, 3.0)
-            self.setupHist(h_ratio_normalized,  self.variable + "_" + region + eraTag, self.label_met, "Data/MC", self.color_black, 0.0, 3.0)
+                # setup histograms
+                #setupHist(self, hist, title, x_title, y_title, color, y_min, y_max)
+                self.setupHist(h_Data,              self.variable + "_" + region + eraTag, self.label_met, "Events", self.color_red,   10.0 ** -1, 10.0 ** 6)
+                self.setupHist(h_MC,                self.variable + "_" + region + eraTag, self.label_met, "Events", self.color_blue,  10.0 ** -1, 10.0 ** 6)
+                self.setupHist(h_MC_normalized,     self.variable + "_" + region + eraTag, self.label_met, "Events", self.color_blue,  10.0 ** -1, 10.0 ** 6)
+                self.setupHist(h_ratio,             self.variable + "_" + region + eraTag, self.label_met, "Data/MC", self.color_black, 0.0, 3.0)
+                self.setupHist(h_ratio_normalized,  self.variable + "_" + region + eraTag, self.label_met, "Data/MC", self.color_black, 0.0, 3.0)
          
-            # Data and MC
-            
-            # draw histograms
-            h_Data.Draw("hist")
-            h_MC.Draw("hist same")
-            # legend: TLegend(x1,y1,x2,y2)
-            legend = ROOT.TLegend(legend_x1, legend_y1, legend_x2, legend_y2)
-            legend.AddEntry(h_Data, "Data", "l")
-            legend.AddEntry(h_MC,   "MC",   "l")
-            legend.Draw()
-            # save histograms
-            c.SetLogy(1) # set log y
-            c.Update()
-            c.SaveAs(plot_name + eraTag + ".pdf")
-            c.SaveAs(plot_name + eraTag + ".png")
-            
-            # Data and Normalized MC
-            
-            # draw histograms
-            h_Data.Draw("hist")
-            h_MC_normalized.Draw("hist same")
-            # legend: TLegend(x1,y1,x2,y2)
-            legend = ROOT.TLegend(legend_x1, legend_y1, legend_x2, legend_y2)
-            legend.AddEntry(h_Data,          "Data",          "l")
-            legend.AddEntry(h_MC_normalized, "Normalized MC", "l")
-            legend.Draw()
-            # save histograms
-            c.SetLogy(1) # set log y
-            c.Update()
-            c.SaveAs(plot_name + "_normalized" + eraTag + ".pdf")
-            c.SaveAs(plot_name + "_normalized" + eraTag + ".png")
-            
-            # Data/MC
+                # Data and MC
+                
+                # draw histograms
+                h_Data.Draw(draw_option)
+                h_MC.Draw(draw_option + " same")
+                # legend: TLegend(x1,y1,x2,y2)
+                legend = ROOT.TLegend(legend_x1, legend_y1, legend_x2, legend_y2)
+                legend.AddEntry(h_Data, "Data", "l")
+                legend.AddEntry(h_MC,   "MC",   "l")
+                legend.Draw()
+                # save histograms
+                c.SetLogy(1) # set log y
+                c.Update()
+                c.SaveAs(plot_name + keyTag + eraTag + ".pdf")
+                c.SaveAs(plot_name + keyTag + eraTag + ".png")
+                
+                # Data and Normalized MC
+                
+                # draw histograms
+                h_Data.Draw(draw_option)
+                h_MC_normalized.Draw(draw_option + " same")
+                # legend: TLegend(x1,y1,x2,y2)
+                legend = ROOT.TLegend(legend_x1, legend_y1, legend_x2, legend_y2)
+                legend.AddEntry(h_Data,          "Data",          "l")
+                legend.AddEntry(h_MC_normalized, "Normalized MC", "l")
+                legend.Draw()
+                # save histograms
+                c.SetLogy(1) # set log y
+                c.Update()
+                c.SaveAs(plot_name + "_normalized" + keyTag + eraTag + ".pdf")
+                c.SaveAs(plot_name + "_normalized" + keyTag + eraTag + ".png")
+                
+                # Data/MC
 
-            # draw histograms
-            h_ratio.Draw("hist")
-            # legend: TLegend(x1,y1,x2,y2)
-            legend = ROOT.TLegend(legend_x1, legend_y1, legend_x2, legend_y2)
-            legend.AddEntry(h_ratio, "Data/MC", "l")
-            legend.Draw()
-            # save histograms
-            c.SetLogy(0) # unset log y
-            c.Update()
-            c.SaveAs(plot_name + "_ratio" + eraTag + ".pdf")
-            c.SaveAs(plot_name + "_ratio" + eraTag + ".png")
-            
-            # Data/(Normalized MC)
-            
-            # draw histograms
-            h_ratio_normalized.Draw("hist")
-            # legend: TLegend(x1,y1,x2,y2)
-            legend = ROOT.TLegend(legend_x1, legend_y1, legend_x2, legend_y2)
-            legend.AddEntry(h_ratio_normalized, "Data/(Normalized MC)", "l")
-            legend.Draw()
-            # save histograms
-            c.SetLogy(0) # unset log y
-            c.Update()
-            c.SaveAs(plot_name + "_ratio_normalized" + eraTag + ".pdf")
-            c.SaveAs(plot_name + "_ratio_normalized" + eraTag + ".png")
+                # draw histograms
+                h_ratio.Draw(draw_option)
+                # legend: TLegend(x1,y1,x2,y2)
+                legend = ROOT.TLegend(legend_x1, legend_y1, legend_x2, legend_y2)
+                legend.AddEntry(h_ratio, "Data/MC", "l")
+                legend.Draw()
+                # save histograms
+                c.SetLogy(0) # unset log y
+                c.Update()
+                c.SaveAs(plot_name + "_ratio" + keyTag + eraTag + ".pdf")
+                c.SaveAs(plot_name + "_ratio" + keyTag + eraTag + ".png")
+                
+                # Data/(Normalized MC)
+                
+                # draw histograms
+                h_ratio_normalized.Draw(draw_option)
+                # legend: TLegend(x1,y1,x2,y2)
+                legend = ROOT.TLegend(legend_x1, legend_y1, legend_x2, legend_y2)
+                legend.AddEntry(h_ratio_normalized, "Data/(Normalized MC)", "l")
+                legend.Draw()
+                # save histograms
+                c.SetLogy(0) # unset log y
+                c.Update()
+                c.SaveAs(plot_name + "_ratio_normalized" + keyTag + eraTag + ".pdf")
+                c.SaveAs(plot_name + "_ratio_normalized" + keyTag + eraTag + ".png")
         
 
 def main():
