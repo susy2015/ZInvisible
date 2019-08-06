097c6f1cb3da47dedb09b5e4770ca2241ef71934
caleb-james-smith <caleb_smith2@baylor.edu>
2019-08-06 17:50:19 -0500

    plot photon data and mc with normalize set to true; add feature to process.py to only make plots and not move or hadd root files

--- Tools/python/process.py
+++ Tools/python/process.py
@@ -9,10 +9,12 @@ def process():
     # options
     parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
     parser.add_argument("--json_file",    "-j", default="",                             help="json file containing runs")
+    parser.add_argument("--plot_only",    "-p", default = False, action = "store_true", help="only make plots; do not move and hadd root files, etc.")
     parser.add_argument("--verbose",      "-v", default = False, action = "store_true", help="verbose flag to print more things")
 
     options     = parser.parse_args()
     json_file   = options.json_file
+    plot_only   = options.plot_only
     verbose     = options.verbose
     
     if not os.path.exists(json_file):
@@ -35,7 +37,11 @@ def process():
         for era in runMap:
             directory = runMap[era]
             print "Processing Era {0}, directory {1}".format(era, directory)
-            command = "./condor/processResults.sh {0} {1}".format(directory, era) 
+            if plot_only:
+                resultFile = "condor/{0}/result.root".format(directory)
+                command = "./makePlots -f -I {0} -Y {1} -R Data_MET_{1} | grep -v LHAPDF".format(resultFile, era) 
+            else:
+                command = "./condor/processResults.sh {0} {1}".format(directory, era) 
             returncode = subprocess.check_call(command, shell=True)
 
 if __name__ == "__main__":
