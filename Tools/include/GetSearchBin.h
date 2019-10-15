51628af551cac93538abab230642a754d1dd8a0b
caleb-james-smith <caleb_smith2@baylor.edu>
2019-10-15 18:21:28 -0500

    Load json file. Place holder functions for passing unit cuts.

--- Tools/include/GetSearchBin.h
+++ Tools/include/GetSearchBin.h
@@ -22,12 +22,20 @@
 #include <iostream>
 #include <string>
 #include <set>
+#include <fstream>
+
+// Repo for json.hpp: https://github.com/nlohmann/json/tree/master
+#include "../../json/single_include/nlohmann/json.hpp"
+using json = nlohmann::json;
 
 namespace plotterFunctions
 {
     class GetSearchBin
     {
     private:
+        
+        // json file containing unit mapping
+        json json_;
 
         void getSearchBin(NTupleReader& tr)
         {
@@ -50,29 +58,35 @@ namespace plotterFunctions
                 
                 float mtb_cut = 175.0;
                 
-                //-----------------------------------------//
-                //--- Updated Search Bins (August 2019) ---//
-                //-----------------------------------------//
+                //------------------------------------------------//
+                //--- Updated Search Bins: SBv4 (October 2019) ---//
+                //------------------------------------------------//
                 // int SBv4_lowdm(int njets, int nb, int nSV, float ISRpt, float bottompt_scalar_sum, float met)
                 // int SBv4_highdm(float mtb, int njets, int nb, int ntop, int nw, int nres, float ht, float met)
                 int nSearchBinLowDM  = SBv4_lowdm(nJets, nBottoms, nSoftBottoms, ISRJetPt, ptb, met);
                 int nSearchBinHighDM = SBv4_highdm(mtb, nJets, nBottoms, nMergedTops, nWs, nResolvedTops, ht, met);
                 
-                //------------------------------------------------//
-                //--- Updated Validation Bins (September 2019) ---//
-                //------------------------------------------------//
+                //------------------------------------------------------//
+                //--- Updated Validation Bins: SBv3 (September 2019) ---//
+                //------------------------------------------------------//
                 // int SBv3_lowdm_validation(int njets, int nb, int nSV, float ISRpt, float bottompt_scalar_sum, float met)
                 // int SBv3_lowdm_validation_high_MET(int nb, int nSV, float ISRpt, float met)
                 // int SBv3_highdm_validation(float mtb, int njets, int ntop, int nw, int nres, int nb, float met)
                 int nValidationBinLowDM        = SBv3_lowdm_validation(nJets, nBottoms, nSoftBottoms, ISRJetPt, ptb, met);
                 int nValidationBinLowDMHighMET = SBv3_lowdm_validation_high_MET(nBottoms, nSoftBottoms, ISRJetPt, met);
                 int nValidationBinHighDM       = SBv3_highdm_validation(mtb, nJets, nMergedTops, nWs, nResolvedTops, nBottoms, met); 
+
+                //----------------------------------------//
+                //--- Updated Unit Bins (October 2019) ---//
+                //----------------------------------------//
+                int nCRUnit = getUnitNum("unitCRNum");
                 
                 tr.registerDerivedVar("nSearchBinLowDM"             + suffix, nSearchBinLowDM);
                 tr.registerDerivedVar("nSearchBinHighDM"            + suffix, nSearchBinHighDM);
                 tr.registerDerivedVar("nValidationBinLowDM"         + suffix, nValidationBinLowDM);
                 tr.registerDerivedVar("nValidationBinLowDMHighMET"  + suffix, nValidationBinLowDMHighMET);
                 tr.registerDerivedVar("nValidationBinHighDM"        + suffix, nValidationBinHighDM);
+                tr.registerDerivedVar("nCRUnit"                     + suffix, nCRUnit);
             }
         }
 
@@ -80,14 +94,45 @@ namespace plotterFunctions
 
         GetSearchBin()
         {
-            // void printJson(const std::string& fileName, const std::string& key, const std::string& title)
+            bool print = false;
             const std::string fileName = "dc_BkgPred_BinMaps_master.json";
-            SusyUtility::printJson(fileName, "binNum",      "Search Bins");
-            SusyUtility::printJson(fileName, "unitCRNum",   "Control Region Units");
-            SusyUtility::printJson(fileName, "unitSRNum",   "Search Region Units");
+            loadJson(fileName);
+            // print json file for testing
+            if (print)
+            {
+                // void printJson(const std::string& fileName, const std::string& key, const std::string& title)
+                SusyUtility::printJson(fileName, "binNum",      "Search Bins");
+                SusyUtility::printJson(fileName, "unitCRNum",   "Control Region Units");
+                SusyUtility::printJson(fileName, "unitSRNum",   "Search Region Units");
+            }
         }
         
         ~GetSearchBin(){}
+        
+        // load json file
+        void loadJson(const std::string& fileName)
+        {
+            // read json file
+            std::ifstream i(fileName);
+            i >> json_;
+        }
+        
+        // return true if event passes unit selection, otherwise return false 
+        bool passUnit(const std::string& unit)
+        {
+            return false;
+        }
+        
+        // return unit number: can be used for search bins, CR units and SR units
+        int getUnitNum(const std::string& key)
+        {
+            for (const auto& element : json_[key].items())
+            {
+                //std::cout << element.key() << " : " << element.value() << std::endl;
+                passUnit(element.key());
+            }
+            return -1;
+        }
 
         void operator()(NTupleReader& tr)
         {
