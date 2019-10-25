78a48b2e1543f3b22a270e52a40c2cd2b928c7ef
Angel Rosado <angel.rosado1@upr.edu>
2019-10-24 19:17:05 -0500

    do loose and mid true

--- Tools/MakePlots.C
+++ Tools/MakePlots.C
@@ -45,7 +45,7 @@ int main(int argc, char* argv[])
     };
     bool runOnCondor        = false;
     bool unblind            = false;
-    bool doLooseAndMid      = false;
+    bool doLooseAndMid      = true;
     bool doDataMCElectron   = true;
     bool doDataMCMuon       = true;
     bool doDataMCPhoton     = true;
