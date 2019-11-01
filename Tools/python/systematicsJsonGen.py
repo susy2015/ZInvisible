f339e50e9df4c1c599e7bb8a3b9deb6ba780347f
caleb-james-smith <caleb_smith2@baylor.edu>
2019-11-01 15:57:24 -0500

    Skip header line that contains keys.

--- Tools/python/systematicsJsonGen.py
+++ Tools/python/systematicsJsonGen.py
@@ -12,6 +12,7 @@ with open("systematics.csv", "r") as csvFile:
         # keys are in row starting with key
         if (row[0] == "key"):
             keys = row
+            continue 
         jsonDict[row[0]] = {}
         # loop over columns to match keys to values
         for i in xrange(1, len(keys)):
