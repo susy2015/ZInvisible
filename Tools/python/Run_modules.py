c8fc326db17c49df5932fc35b2724cdbb55a43dd
angel <angel.rosado1@upr.edu>
2019-12-07 02:19:17 -0600

    loop over systematics in Run_modules.py

--- Tools/python/Run_modules.py
+++ Tools/python/Run_modules.py
@@ -17,6 +17,7 @@ out_dir      =  "validation/"
 result_file  =  "result.root"
 regions      =  ["lowdm", "highdm"]
 directions   =  ['up', '', 'down']
+systematics  =  ['btag','eff_restoptag','eff_sb','eff_toptag','eff_wtag','met_trig','pileup'] # all systematics available from Znunu_nValidationBin
 
 histo = {region:dict.fromkeys(directions) for region in regions} # histo[regioin][direction]
 
@@ -26,7 +27,7 @@ histo = {region:dict.fromkeys(directions) for region in regions} # histo[regioin
 
 N = Normalization(out_dir, verbose)
 S = Shape(out_dir, draw, doUnits, verbose)
-VB = ValidationBins(N, S, era, out_dir, True)
+VB = ValidationBins(N, S, era, out_dir, verbose)
 
 #-------------------------------------------------------
 # Normal predictions (no systematics) 
@@ -43,79 +44,74 @@ histo["highdm"][""]  =  VB.histograms[era]["highdm"]["mc"].Clone()
 # Calculate normalization and shape factors
 #-------------------------------------------------------
 
-#syst = "btag"
-#syst = "eff_sb_photon"
-#syst = "eff_sb"
-#syst = "pileup"
-#syst = "met_trig"
-syst = "eff_toptag"
+for syst in systematics:
 
-N.getNormAndError(result_file, syst + "_syst_up", era)
-S.getShape(result_file, syst + "_syst_up", era)
-print("Everything is fine so far")
-VB.getValues(result_file, syst + "_syst_up", era)
-
-histo["highdm"]["up"]  =  VB.histograms[era]["highdm"]["mc"].Clone()
-histo["lowdm"]["up"]   =  VB.histograms[era]["lowdm"]["mc"].Clone()
-
-N.getNormAndError(result_file, syst + "_syst_down", era)
-S.getShape(result_file, syst + "_syst_down", era)
-VB.getValues(result_file, syst + "_syst_down", era)
-
-histo["highdm"]["down"]  =  VB.histograms[era]["highdm"]["mc"].Clone()
-histo["lowdm"]["down"]   =  VB.histograms[era]["lowdm"]["mc"].Clone()
-
-#-------------------------------------------------------
-# Canvas 
-#-------------------------------------------------------
-
-for region in regions:
-
-    # legend: TLegend(x1,y1,x2,y2)
-    legend_x1 = 0.7 
-    legend_x2 = 0.9 
-    legend_y1 = 0.7 
-    legend_y2 = 0.9 
-    legend = ROOT.TLegend(legend_x1, legend_y1, legend_x2, legend_y2)
-
-    c = ROOT.TCanvas("c", "c", 800, 800)
-    c.Divide(1, 2)
-
-    c.cd(1)
+    N.getNormAndError(result_file, syst + "_syst_up", era)
+    S.getShape(result_file, syst + "_syst_up", era)
+    print("Everything is fine so far")
+    VB.getValues(result_file, syst + "_syst_up", era)
     
-    histo[region][""].Draw("hist")
-    histo[region][""].SetLineColor(ROOT.kBlue)
+    histo["highdm"]["up"]  =  VB.histograms[era]["highdm"]["mc"].Clone()
+    histo["lowdm"]["up"]   =  VB.histograms[era]["lowdm"]["mc"].Clone()
     
-    histo[region]["up"].Draw("same")
-    histo[region]["up"].SetLineColor(ROOT.kRed)
+    N.getNormAndError(result_file, syst + "_syst_down", era)
+    S.getShape(result_file, syst + "_syst_down", era)
+    VB.getValues(result_file, syst + "_syst_down", era)
     
-    histo[region]["down"].Draw("same")
-    histo[region]["down"].SetLineColor(ROOT.kViolet)
+    histo["highdm"]["down"]  =  VB.histograms[era]["highdm"]["mc"].Clone()
+    histo["lowdm"]["down"]   =  VB.histograms[era]["lowdm"]["mc"].Clone()
     
-    ROOT.gPad.SetLogy(1) # set log y
+    #-------------------------------------------------------
+    # Canvas 
+    #-------------------------------------------------------
     
-    legend.AddEntry(histo[region]["up"]   , "up"      , "l")
-    legend.AddEntry(histo[region][""]     , "nominal" , "l")
-    legend.AddEntry(histo[region]["down"] , "down"    , "l")
-    legend.Draw()
+    for region in regions:
     
-    c.cd(2)
-
-    ratio_up = histo[region]["up"].Clone()
-    ratio_up.Divide(histo[region][""])
-    ratio_up.SetLineColor(ROOT.kRed)
-
-    ratio_down = histo[region]["down"].Clone()
-    ratio_down.Divide(histo[region][""])
-    ratio_down.SetLineColor(ROOT.kViolet)
-
-    ratio_up.GetYaxis().SetRangeUser(0,1.5)
-
-    ratio_up.Draw("hist")
-    ratio_down.Draw("hist same")
+        # legend: TLegend(x1,y1,x2,y2)
+        legend_x1 = 0.7 
+        legend_x2 = 0.9 
+        legend_y1 = 0.7 
+        legend_y2 = 0.9 
+        legend = ROOT.TLegend(legend_x1, legend_y1, legend_x2, legend_y2)
     
-    c.Update()
+        c = ROOT.TCanvas("c", "c", 800, 800)
+        c.Divide(1, 2)
     
-    file_name = "Syst_" + syst + "_" + region
-    c.SaveAs(file_name + ".png")
+        c.cd(1)
+        
+        histo[region][""].Draw("hist")
+        histo[region][""].SetLineColor(ROOT.kBlue)
+        
+        histo[region]["up"].Draw("same")
+        histo[region]["up"].SetLineColor(ROOT.kRed)
+        
+        histo[region]["down"].Draw("same")
+        histo[region]["down"].SetLineColor(ROOT.kViolet)
+        
+        ROOT.gPad.SetLogy(1) # set log y
+        
+        legend.AddEntry(histo[region]["up"]   , "up"      , "l")
+        legend.AddEntry(histo[region][""]     , "nominal" , "l")
+        legend.AddEntry(histo[region]["down"] , "down"    , "l")
+        legend.Draw()
+        
+        c.cd(2)
+    
+        ratio_up = histo[region]["up"].Clone()
+        ratio_up.Divide(histo[region][""])
+        ratio_up.SetLineColor(ROOT.kRed)
+    
+        ratio_down = histo[region]["down"].Clone()
+        ratio_down.Divide(histo[region][""])
+        ratio_down.SetLineColor(ROOT.kViolet)
+    
+        ratio_up.GetYaxis().SetRangeUser(0,1.5)
+    
+        ratio_up.Draw("hist")
+        ratio_down.Draw("hist same")
+        
+        c.Update()
+        
+        file_name = "Syst_" + syst + "_" + region
+        c.SaveAs(file_name + ".png")
 
