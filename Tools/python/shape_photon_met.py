57b93abb61c258e7667a27a436ab33969b08d2ef
caleb-james-smith <caleb_smith2@baylor.edu>
2020-02-03 16:40:34 -0600

    remove old version of shape comparison plot. print chi 2 and reduced chi 2

--- Tools/python/shape_photon_met.py
+++ Tools/python/shape_photon_met.py
@@ -473,9 +473,6 @@ class Shape:
         for region in self.regions:
             for selection in self.selections[bin_type][region]:
                 for rebin in self.ratio_rebinned_map["2016"][bin_type][region][selection]: 
-                    
-                    # ---------------- new version --------------------------
-                    
                     title = "Shape for {0} bins, {1}, {2}, {3}".format(bin_type, region, selection, rebin)
                     x_title = "MET (GeV)" 
                     y_title = "Shape #left(S_{#gamma}#right)"
@@ -509,57 +506,6 @@ class Shape:
                     c.SaveAs(plot_name + ".pdf")
                     c.SaveAs(plot_name + ".png")
                     
-                    # ---------------- old version --------------------------
-                    
-                    if False:
-                        h1 = self.ratio_rebinned_map["2016"][bin_type][region][selection][rebin]
-                        h2 = self.ratio_rebinned_map["2017_BE"][bin_type][region][selection][rebin]
-                        h3 = self.ratio_rebinned_map["2017_F"][bin_type][region][selection][rebin]
-                        h4 = self.ratio_rebinned_map["2018_PreHEM"][bin_type][region][selection][rebin]
-                        h5 = self.ratio_rebinned_map["2018_PostHEM"][bin_type][region][selection][rebin]
-                        h6 = self.ratio_rebinned_map["Run2"][bin_type][region][selection][rebin]
-                        title = "Shape for {0} bins, {1}, {2}, {3}".format(bin_type, region, selection, rebin)
-                        x_title = "MET (GeV)" 
-                        y_title = "Shape #left(S_{#gamma}#right)"
-                        y_min = -1.0
-                        y_max = 3.0
-                        #setupHist(hist, title, x_title, y_title, color, y_min, y_max)
-                        setupHist(h1,   title, x_title, y_title, "pinkish red",     y_min, y_max)
-                        setupHist(h2,   title, x_title, y_title, "tangerine",       y_min, y_max)
-                        setupHist(h3,   title, x_title, y_title, "emerald",         y_min, y_max)
-                        setupHist(h4,   title, x_title, y_title, "dark sky blue",   y_min, y_max)
-                        setupHist(h5,   title, x_title, y_title, "pinky purple",    y_min, y_max)
-                        setupHist(h6,   title, x_title, y_title, "black",           y_min, y_max)
-                        # draw
-                        h1.Draw(draw_option)
-                        h2.Draw(draw_option + " same")
-                        h3.Draw(draw_option + " same")
-                        h4.Draw(draw_option + " same")
-                        h5.Draw(draw_option + " same")
-                        h6.Draw(draw_option + " same")
-                        # legend: TLegend(x1,y1,x2,y2)
-                        legend = ROOT.TLegend(legend_x1, legend_y1, legend_x2, legend_y2)
-                        legend.AddEntry(h1,     "2016",             "l")
-                        legend.AddEntry(h2,     "2017_BE",          "l")
-                        legend.AddEntry(h3,     "2017_F",           "l")
-                        legend.AddEntry(h4,     "2018_PreHEM",      "l")
-                        legend.AddEntry(h5,     "2018_PostHEM",     "l")
-                        legend.AddEntry(h6,     "Run2",             "l")
-                        legend.Draw()
-                        # save histograms
-                        plot_name = "{0}Shape_{1}_{2}_{3}_{4}".format(self.plot_dir, bin_type, region, selection, rebin)
-                        c.Update()
-                        c.SaveAs(plot_name + ".pdf")
-                        c.SaveAs(plot_name + ".png")
-               
-                        # delete histograms to avoid memory leak
-                        del h1
-                        del h2
-                        del h3
-                        del h4
-                        del h5
-                        del h6
-                
 
 def main():
     json_file = "runs/run_2019-07-17.json"
