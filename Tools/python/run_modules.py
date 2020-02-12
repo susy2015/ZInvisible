2fa97f43319c725130deba3053820c97c6e6e7e2
caleb-james-smith <caleb_smith2@baylor.edu>
2020-02-12 15:02:16 -0600

    get Rz and Z vs Photon systematics for MET study bins

--- Tools/python/run_modules.py
+++ Tools/python/run_modules.py
@@ -93,8 +93,10 @@ def main():
     N.makeTexFile("validation", latex_dir + "validationBins_normalization_Zmass.tex")
     N.makeTexFile("search",     latex_dir + "searchBins_normalization_Zmass.tex")
     N.makeComparison("validation")
+    N.makeComparison("validationMetStudy")
     N.makeComparison("search")
     S.makeComparison("validation")
+    S.makeComparison("validationMetStudy")
     S.makeComparison("search")
     VB.makeTexFile("Z Invisible Per Era Prediction for Validation Bins", latex_dir + "zinv_per_era_prediction_validation_bins.tex")
     SB.makeTexFile("Z Invisible Per Era Prediction for Search Bins",     latex_dir + "zinv_per_era_prediction_search_bins.tex")
@@ -112,10 +114,12 @@ def main():
     # Get systematics in proper bins: Rz and "Z to LL vs. Photon" systematics
     # must be done after N.makeComparison()
     # must be done after Syst.makeZvsPhoton()
-    VB.getRzSyst(N.rz_syst_map, "validation",   "RzSyst_ValidationBins.root")
-    SB.getRzSyst(N.rz_syst_map, "search",       "RzSyst_SearchBins.root")
-    VB.getZvsPhotonSyst(Syst.h_map_syst,        "ZvsPhotonSyst_ValidationBins.root")
-    SB.getZvsPhotonSyst(Syst.h_map_syst,        "ZvsPhotonSyst_SearchBins.root")
+    VB.getRzSyst(           N.rz_syst_map,      "validation",           "RzSyst_ValidationBins.root")
+    VB_MS.getRzSyst(        N.rz_syst_map,      "validationMetStudy",   "RzSyst_ValidationBinsMETStudy.root")
+    SB.getRzSyst(           N.rz_syst_map,      "search",               "RzSyst_SearchBins.root")
+    VB.getZvsPhotonSyst(    Syst.h_map_syst,                            "ZvsPhotonSyst_ValidationBins.root")
+    VB_MS.getZvsPhotonSyst( Syst.h_map_syst,                            "ZvsPhotonSyst_ValidationBinsMETStudy.root")
+    SB.getZvsPhotonSyst(    Syst.h_map_syst,                            "ZvsPhotonSyst_SearchBins.root")
     CRunits.getZvsPhotonSyst(Syst.h_map_syst,   "ZvsPhotonSyst_CRUnitBins.root")
 
     if doUnits:
