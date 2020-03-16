a412a0c6b2620ccce994938e0c58bbeb9c7fb7a3
caleb-james-smith <caleb_smith2@baylor.edu>
2020-03-16 16:30:57 -0500

    load event branch and print

--- Tools/skimmer.cc
+++ Tools/skimmer.cc
@@ -22,21 +22,23 @@ int main(int argc, char *argv[])
     //Get old file, old tree and set top branch address
     TFile *oldfile = TFile::Open("root://cmseos.fnal.gov//store/user/lpcsusyhad/Stop_production/Autumn18_102X_v1/PostProcessed_22March2019_v6/TTGJets_2018/TTGJets_2018_0.root");
     TTree *oldtree = (TTree*)oldfile->Get("Events");
-     
     Int_t nentries = (Int_t)oldtree->GetEntries();
     
     std::cout << "nEvents before skimming: " << nentries << std::endl;
     
+    // branches
+    ULong64_t *event = 0;
+    oldtree->SetBranchAddress("event", &event);
+
     // //Create a new file + a clone of old tree in new file
     TFile *newfile = new TFile("TTGJets_2018_0_skimmed.root","recreate");
     TTree *newtree = oldtree->CloneTree(0);
     
-    // //for (Int_t i=0;i<nentries; i++) {
-    // for (Int_t i=0;i<1000; i++) {
-    //    oldtree->GetEntry(i);
-    //    if (event->GetNtrack() > 605) newtree->Fill();
-    //    event->Clear();
-    // }
+    //for (Int_t i=0;i<nentries; i++) {
+    for (Int_t i=0;i<1000; i++) {
+       oldtree->GetEntry(i);
+       printf("i = %d, event = %d\n", i, event);
+    }
     
     //newtree->Print();
     newtree->AutoSave();
