ff02cb0790b307070bed7a627f4a4ba806a2528b
caleb-james-smith <caleb_smith2@baylor.edu>
2020-04-08 12:52:59 -0500

    comment syst. prints

--- Tools/MakePlots.C
+++ Tools/MakePlots.C
@@ -2387,7 +2387,7 @@ int main(int argc, char* argv[])
                     vh.push_back(PHS("DataMC_Photon_HighDM_nCRUnitHighDM" + histSuffixSyst,              {dcData_Photon_HighDM_nCRUnitHighDM,                   dcMC_Photon_HighDM_nCRUnitHighDM},                   {1, 2}, "", max_crunit_high_dm - min_crunit_high_dm,  min_crunit_high_dm, max_crunit_high_dm,  true, false, "Control Region Unit High DM", "Events"));
                 }
                 
-                printf("# Systematics for Photon histograms\n");
+                //printf("# Systematics for Photon histograms\n");
                 for (const auto& element : systematics_json.items())
                 {
                     std::string syst            = element.key();
@@ -2403,10 +2403,10 @@ int main(int argc, char* argv[])
                         std::string PhotonWeights_down   = PhotonWeights;
                         PhotonWeights_up.replace(    pos, len, syst_up);
                         PhotonWeights_down.replace(  pos, len, syst_down);
-                        printf("- %s, %s: (%s, %s, %s)\n", syst.c_str(), syst_name.c_str(), syst_nominal.c_str(), syst_up.c_str(), syst_down.c_str());
-                        printf("\tnominal weights: %s\n",   PhotonWeights.c_str());
-                        printf("\tsyst_up weights: %s\n",   PhotonWeights_up.c_str());
-                        printf("\tsyst_down weights: %s\n", PhotonWeights_down.c_str());
+                        //printf("- %s, %s: (%s, %s, %s)\n", syst.c_str(), syst_name.c_str(), syst_nominal.c_str(), syst_up.c_str(), syst_down.c_str());
+                        //printf("\tnominal weights: %s\n",   PhotonWeights.c_str());
+                        //printf("\tsyst_up weights: %s\n",   PhotonWeights_up.c_str());
+                        //printf("\tsyst_down weights: %s\n", PhotonWeights_down.c_str());
                         std::map<std::string, std::string> weightMap;
                         weightMap["up"]     = PhotonWeights_up;
                         weightMap["down"]   = PhotonWeights_down;
@@ -3814,7 +3814,7 @@ int main(int argc, char* argv[])
                     vh.push_back(PHS("ZNuNu_nSRUnit_LowDM" + histSuffixSyst,                    {dcMC_ZNuNu_nSRUnit_LowDM},                     {1, 1}, "", max_srunit_low_dm - min_srunit_low_dm,              min_srunit_low_dm,          max_srunit_low_dm,          false, false,  "Search Region Unit Low DM", "Events", true));
                     vh.push_back(PHS("ZNuNu_nSRUnit_HighDM" + histSuffixSyst,                   {dcMC_ZNuNu_nSRUnit_HighDM},                    {1, 1}, "", max_srunit_high_dm - min_srunit_high_dm,            min_srunit_high_dm,         max_srunit_high_dm,         false, false,  "Search Region Unit High DM", "Events", true));
                 }
-                printf("# Systematics for Z nu nu histograms\n");
+                //printf("# Systematics for Z nu nu histograms\n");
                 for (const auto& element : systematics_json.items())
                 {
                     std::string syst            = element.key();
@@ -3830,10 +3830,10 @@ int main(int argc, char* argv[])
                         std::string ZNuNuWeights_down   = ZNuNuWeights;
                         ZNuNuWeights_up.replace(    pos, len, syst_up);
                         ZNuNuWeights_down.replace(  pos, len, syst_down);
-                        printf("- %s, %s: (%s, %s, %s)\n", syst.c_str(), syst_name.c_str(), syst_nominal.c_str(), syst_up.c_str(), syst_down.c_str());
-                        printf("\tnominal weights: %s\n",   ZNuNuWeights.c_str());
-                        printf("\tsyst_up weights: %s\n",   ZNuNuWeights_up.c_str());
-                        printf("\tsyst_down weights: %s\n", ZNuNuWeights_down.c_str());
+                        //printf("- %s, %s: (%s, %s, %s)\n", syst.c_str(), syst_name.c_str(), syst_nominal.c_str(), syst_up.c_str(), syst_down.c_str());
+                        //printf("\tnominal weights: %s\n",   ZNuNuWeights.c_str());
+                        //printf("\tsyst_up weights: %s\n",   ZNuNuWeights_up.c_str());
+                        //printf("\tsyst_down weights: %s\n", ZNuNuWeights_down.c_str());
                         std::map<std::string, std::string> weightMap;
                         weightMap["up"]     = ZNuNuWeights_up;
                         weightMap["down"]   = ZNuNuWeights_down;
