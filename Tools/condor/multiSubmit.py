e45babd75e6ff497e691564b8aaa1451956fab2e
caleb-james-smith <caleb_smith2@baylor.edu>
2020-05-10 23:03:54 -0500

    only submit 2018 due to eos issues

--- Tools/condor/multiSubmit.py
+++ Tools/condor/multiSubmit.py
@@ -39,7 +39,8 @@ def main():
             "2018_PostHEM"  : 1
     }
     #eras = ["2016", "2017_BE", "2017_F", "2018_PreHEM", "2018_PostHEM"]
-    eras = ["2016", "2017", "2018"]
+    #eras = ["2016", "2017", "2018"]
+    eras = ["2018"]
     dirMap = {}
     # submit jobs for each era 
     for era in eras:
