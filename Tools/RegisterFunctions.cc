606ff1e244b8dcbdbabb3e9cc948f7c318350f6d
caleb-james-smith <caleb_smith2@baylor.edu>
2020-05-30 14:57:11 -0500

    Remove delete topWeightCalculator as it may be causing failures for condor jobs

--- Tools/RegisterFunctions.cc
+++ Tools/RegisterFunctions.cc
@@ -213,7 +213,6 @@ void RegisterFunctionsNTuple::setupTopWeightCalculator(NTupleReader& tr)
     {
         std::string TopTagger_effFile   = "tTagEff_" + year_ + ".root";
         // TopWeightCalculator(const std::string& effFileName, const std::string& sampleName, const std::string& year)
-        if(topWeightCalculator) delete topWeightCalculator;
         topWeightCalculator = new TopWeightCalculator(TopTagger_effFile, sampleName_, year_);
         tr.registerFunction(*topWeightCalculator);
     }
