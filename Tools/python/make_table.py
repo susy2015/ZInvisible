f9579723c38ff617139cba7fc19eafc93b1e7262
caleb-james-smith <caleb_smith2@baylor.edu>
2020-06-02 10:19:29 -0500

    fix typo, use zinv command

--- Tools/python/make_table.py
+++ Tools/python/make_table.py
@@ -353,7 +353,7 @@ class Table:
                 if lastBin >= 53:
                     region = "high \dm"
                 label = "tab:zinvPredToBin{0}".format(lastBin)
-                caption  = "Predition for the Z to invisible background $\\left(\Np\\right)$ in {0} search bins {1}--{2}.".format(region, firstBin, lastBin)
+                caption  = "Prediction for the \zinv background $\\left(\Np\\right)$ in {0} search bins {1}--{2}.".format(region, firstBin, lastBin)
                 caption += " The normalization factor \Rz, shape factor \Sg, and number of \znunu MC events \Nmc are also shown for each search bin including their statistical uncertainties."
                 caption += " The uncertainty for the prediction $\\left(\Np\\right)$ is calculated by propagating the statistical uncertainties of \Rz, \Sg, and \znunu MC."
                 caption += " See Eq.~\\ref{eq:zinv_pred}."
